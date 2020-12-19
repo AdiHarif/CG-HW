56cba4312002d666e08847e45531e488c19d09d0
Adi Harif <adi.harif@campus.technion.ac.il>
2020-12-19 18:06:08 +0200

    fixed gitignore for real this time

--- CG_skel_w_MFC/resource.h
+++ CG_skel_w_MFC/resource.h
@@ -3,14 +3,79 @@
 // Used by CG_skel_w_MFC.rc
 //
 #define IDS_APP_TITLE                   103
+#define IDD_DIALOG_NEW_CAMERA           112
+#define IDD_DIALOG_EDIT_ACTIVE_CAMERA   116
+#define IDD_DIALOG_TRANSFORM_ACTIVE_MODEL 118
+#define IDC_EDIT_POS_X                  1015
+#define IDC_STATIC_X                    1016
+#define IDC_STATIC_POS                  1017
+#define IDC_EDIT_LOOKAT_X               1018
+#define IDC_STATIC_Y                    1019
+#define IDC_EDIT_POS_Y                  1020
+#define IDC_STATIC_Z                    1021
+#define IDC_EDIT_POS_Z                  1022
+#define IDC_STATIC_LOOKAT               1023
+#define IDC_EDIT_LOOKAT_Y               1024
+#define IDC_EDIT_LOOKAT_Z               1025
+#define IDC_STATIC_X2                   1026
+#define IDC_STATIC_Y2                   1027
+#define IDC_STATIC_Z2                   1028
+#define IDC_STATIC_UP                   1029
+#define IDC_STATIC_Z_FAR                1030
+#define IDC_EDIT_UP_X                   1030
+#define IDC_EDIT_UP_Y                   1031
+#define IDC_EDIT_UP_Z                   1032
+#define IDC_STATIC_X3                   1033
+#define IDC_STATIC_Y3                   1034
+#define IDC_STATIC_Z3                   1035
+#define IDC_STATIC_TRANSLATE_X          1048
+#define IDC_EDIT_TRANSLATE_X            1049
+#define IDC_STATIC_TRANSLATE_Y          1050
+#define IDC_EDIT_TRANSLATE_Y            1051
+#define IDC_STATIC_TRANSLATE_Z          1052
+#define IDC_EDIT_TRANSLATE_Z            1053
+#define IDC_STATIC_ROTATE_X             1054
+#define IDC_EDIT_ROTATE_X               1055
+#define IDC_STATIC_ROTATE_Y             1056
+#define IDC_EDIT_ROTATE_Y               1057
+#define IDC_STATIC_ORDER                1058
+#define IDC_EDIT_ROTATE_Z               1059
+#define IDC_RADIO_ORTHO                 1060
+#define IDC_EDIT_SCALE_X                1060
+#define IDC_STATIC_GROUP_TYPE           1061
+#define IDC_EDIT_SCALE_Y                1061
+#define IDC_COMBO_ORDER                 1062
+#define IDC_STATIC_ROTATE_Z2            1063
+#define IDC_STATIC_TOP                  1064
+#define IDC_EDIT_TRANSLATE_Z2           1064
+#define IDC_EDIT_SCALE_Z                1064
+#define IDC_STATIC_SCALE_X              1065
+#define IDC_STATIC_SCALE_Y              1066
+#define IDC_STATIC_SCALE_Z              1067
+#define IDC_RADIO_FRUSTUM               1084
+#define IDC_RADIO_PERSPECTIVE           1085
+#define IDC_STATIC_FOVY                 1092
+#define IDC_EDIT_FOVY                   1093
+#define IDC_STATIC_ASPECT               1094
+#define IDC_EDIT_ASPECT                 1095
+#define IDC_STATIC_LEFT                 1096
+#define IDC_EDIT_LEFT                   1097
+#define IDC_STATIC_RIGHT                1098
+#define IDC_EDIT_RIGHT                  1099
+#define IDC_STATIC_BOTTOM               1100
+#define IDC_EDIT_BOTTOM                 1101
+#define IDC_EDIT_TOP                    1103
+#define IDC_STATIC_Z_NEAR               1104
+#define IDC_EDIT_Z_NEAR                 1105
+#define IDC_EDIT_Z_FAR                  1106
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        120
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1065
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
